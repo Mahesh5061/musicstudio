459225240e7f231344a98d6c31a03c326ecf5da4
Mahesh5061 <119738289+Mahesh5061@users.noreply.github.com>
2023-12-19 16:35:24 +0530

    HeaderChangeswithnavbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { Route} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import Header from '../src/HomePageComponents/Header';
 
-function App() {
+const Home = () => <div>Home Page</div>;
+const About = () => <div>About Page</div>;
+const Contact = () => <div>Contact Page</div>;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+  <Routes>
+    <Header/>
+  <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+  </Routes>
+  </BrowserRouter>
   );
-}
+};
 
 export default App;
